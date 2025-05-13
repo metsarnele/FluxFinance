97cc95d84162c2c034ecd0e91d6faa06bfe52e73
Nele Metsar <nele.metsar@vikk.ee>
2025-05-13 14:58:42 +0300

    Update sign-in modal background

--- client/src/views/SignInView.vue
+++ client/src/views/SignInView.vue
@@ -103,7 +103,7 @@ export default {
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: #2c3e50;
   display: flex;
   justify-content: center;
   align-items: center;
